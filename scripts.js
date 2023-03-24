9cf4ebb65dd8064d144a87e0caf73b6254ff6ad5
jotafich <123663027+jotafich@users.noreply.github.com>
2023-03-24 19:44:51 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,163 +1,146 @@
-// Funciones para manejar el almacenamiento en localStorage
+// Inicializar Firebase
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  databaseURL: "TU_DATABASE_URL",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID",
+  measurementId: "TU_MEASUREMENT_ID"
+};
+firebase.initializeApp(firebaseConfig);
+
+// Referencia a la base de datos
+const database = firebase.database();
+
+// Funciones para manejar el almacenamiento en Firebase
 function saveUserData(data) {
-    localStorage.setItem("userData", JSON.stringify(data));
+  database.ref('userData').set(data);
 }
 
 function getUserData() {
-    return JSON.parse(localStorage.getItem("userData"));
+  return new Promise((resolve, reject) => {
+    database.ref('userData').once('value').then(snapshot => {
+      resolve(snapshot.val());
+    }).catch(error => {
+      reject(error);
+    });
+  });
 }
 
 function saveEpic(epic) {
-    console.log(epic); // Agregado para verificar si se está guardando correctamente la épica
-    const epics = getEpics();
-    const index = epics.findIndex(item => item.id === epic.id);
-    if (index === -1) {
-      epics.push(epic);
-    } else {
-      epics[index] = epic;
-    }
-    localStorage.setItem("epics", JSON.stringify(epics));
-  }
-  
-    
-  function getEpics() {
-    const epics = JSON.parse(localStorage.getItem("epics")) || [];
-    console.log(epics); // Agregado para verificar si estamos obteniendo un array de épicas
-    return epics;
-  }
-  
+  database.ref(`epics/${epic.id}`).set(epic);
+}
+
+function getEpics() {
+  return new Promise((resolve, reject) => {
+    database.ref('epics').once('value').then(snapshot => {
+      const epics = [];
+      snapshot.forEach(childSnapshot => {
+        epics.push(childSnapshot.val());
+      });
+      resolve(epics);
+    }).catch(error => {
+      reject(error);
+    });
+  });
+}
 
 function deleteEpic(epicId) {
-    const epics = getEpics();
-    const filteredEpics = epics.filter(epic => epic.id !== epicId);
-    localStorage.setItem("epics", JSON.stringify(filteredEpics));
+  database.ref(`epics/${epicId}`).remove();
 }
 
 // Funciones para manejar el formulario de datos de usuario
 function setupUserDataForm() {
-    const form = document.getElementById("user-data-form");
-    form.addEventListener("submit", handleUserDataSubmit);
+  const form = document.getElementById("user-data-form");
+  form.addEventListener("submit", handleUserDataSubmit);
 
-    const expandButton = document.getElementById("expand-user-data");
-    expandButton.addEventListener("click", toggleUserDataForm);
+  const expandButton = document.getElementById("expand-user-data");
+  expandButton.addEventListener("click", toggleUserDataForm);
 }
-
 
 function handleUserDataSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const userData = {
-        nombre: formData.get("nombre"),
-        apellido: formData.get("apellido"),
-        squad: formData.get("squad"),
-        quarter: formData.get("quarter")
-    };
-    saveUserData(userData);
-    showSuccessMessage();
-    toggleUserDataForm();
-}
-
-function showSuccessMessage() {
-    const message = document.createElement("div");
-    message.className = "success-message";
-    message.textContent = "Los datos fueron ingresados exitosamente";
-    document.body.appendChild(message);
-    setTimeout(() => {
-        message.remove();
-    }, 3000);
-}
-
-function toggleUserDataForm() {
-    const userDataForm = document.querySelector(".user-data");
-    userDataForm.classList.toggle("collapsed");
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const userData = {
+    nombre: formData.get("nombre"),
+    apellido: formData.get("apellido"),
+    squad: formData.get("squad"),
+    quarter: formData.get("quarter")
+  };
+  saveUserData(userData);
+  showSuccessMessage();
+  toggleUserDataForm();
 }
 
 // Funciones para manejar las tarjetas de épicas
 function setupEpicCards() {
-    const epics = getEpics();
+  getEpics().then(epics => {
     epics.forEach(renderEpicCard);
+  });
 }
 
 function renderEpicCard(epic) {
-    const cardContainer = document.querySelector(".epic-cards");
-    const card = document.createElement("div");
-    card.className = "epic-card";
-    card.dataset.id = epic.id;
-  
-    card.innerHTML = `
-        <h3>${epic.number} - ${epic.name}</h3>
-        <p>Squads con dependencias: ${epic.dependentSquads}</p>
-        <p>Fecha de inicio: ${epic.startDate}</p>
-        <p>Fecha de implementación: ${epic.implementationDate}</p>
-        <div class="dependency-status">
-        <input type="checkbox" id="dependency-${epic.id}" ${epic.hasOwnProperty('dependenciesResolved') && epic.dependenciesResolved ? 'checked' : ''}>
-        <label for="dependency-${epic.id}">Dependencias ${getDependencyStatus(epic)}</label>
-        </div>
-        <button class="delete-epic">Eliminar</button>
-        `;
-  
-    card.querySelector(".delete-epic").addEventListener("click", handleDeleteEpicClick);
-    const checkbox = card.querySelector("input[type='checkbox']");
-    checkbox.addEventListener("change", handleDependencyStatusChange);
-    cardContainer.appendChild(card);
+  const cardContainer = document.querySelector(".epic-cards");
+  const card = document.createElement("div");
+  card.className = "epic-card";
+  card.dataset.id = epic.id;
+
+  card.innerHTML = `
+    <h3>${epic.number} - ${epic.name}</h3>
+    <p>Squads con dependencias: ${epic.dependentSquads}</p>
+    <p>Fecha de inicio: ${epic.startDate}</p>
+    <p>Fecha de implementación: ${epic.implementationDate}</p>
+    <div class="dependency-status">
+      <input type="checkbox" id="dependency-${epic.id}" ${epic.hasOwnProperty('dependenciesResolved') && epic.dependenciesResolved ? 'checked' : ''}>
+      <label for="dependency-${epic.id}">Dependencias ${getDependencyStatus(epic)}</label>
+    </div>
+    <button class="delete-epic">Eliminar</button>
+  `;
+
+  card.querySelector(".delete-epic").addEventListener("click", handleDeleteEpicClick);
+  const checkbox = card.querySelector("input[type='checkbox']");
+  checkbox.addEventListener("change", handleDependencyStatusChange);
+  cardContainer.appendChild(card);
   }
   
-
-
   function handleDependencyStatusChange(e) {
     const epicid = e.target.closest(".epic-card").dataset.id;
-    const epics = getEpics();
-    const epic = epics.find(epic => epic.id == epicid);
-    if (epic) {
-        epic.dependenciesResolved = e.target.checked;
-        localStorage.setItem("epics", JSON.stringify(epics)); // Guardar el arreglo actualizado en localStorage
-        updateDependencyStatus(e.target);
-        const sortedEpics = epics.sort((a, b) => new Date(a.implementationDate) - new Date(b.implementationDate));
-        renderEpicTable(sortedEpics);
-    } else {
-        console.error(`No se encontró una épica con id ${epicid}`);
-    }
-}
+    const epicsRef = firebase.database().ref('epics');
+    epicsRef.child(epicid).update({dependenciesResolved: e.target.checked}, (error) => {
+      if (error) {
+        console.log("Error updating epic: ", error);
+      } else {
+        console.log("Epic updated successfully!");
+      }
+    });
+  }
 
-  
-
-function updateDependencyStatus(checkbox) {
-    const epicId = checkbox.closest(".epic-card").dataset.id;
-    const epics = getEpics();
-    const epic = epics.find(epic => epic.id == epicId);
-    if (epic) {
-        epic.dependenciesResolved = checkbox.checked;
-        localStorage.setItem("epics", JSON.stringify(epics)); // Guardar el arreglo actualizado en localStorage
-        const status = epic.dependenciesResolved ? "OK" : "PENDIENTE";
-        const label = checkbox.nextElementSibling;
-        label.style.color = checkbox.checked ? "green" : "red";
-        label.textContent = `Dependencias ${status}`;
-    } else {
-        console.error(`No se encontró una épica con ID ${epicId}`);
-    }
-}
-
-  
-
-function getDependencyStatus(epic) {
+  function getDependencyStatus(epic) {
     return epic.dependenciesResolved ? "OK" : "PENDIENTE";
-}
+  }
 
+  function handleDeleteEpicClick(e) {
+    const card = e.target.closest(".epic-card");
+    const epicId = parseInt(card.dataset.id, 10);
+    const epicsRef = firebase.database().ref('epics');
+    epicsRef.child(epicId).remove((error) => {
+      if (error) {
+        console.log("Error deleting epic: ", error);
+      } else {
+        console.log("Epic deleted successfully!");
+      }
+    });
+    card.remove();
+  }
 
-function handleDeleteEpicClick(e) {
-const card = e.target.closest(".epic-card");
-const epicId = parseInt(card.dataset.id, 10);
-deleteEpic(epicId);
-card.remove();
-}
+  function setupAddEpicForm() {
+    const form = document.getElementById("add-epic-form");
+    form.addEventListener("submit", handleAddEpicSubmit);
+  }
 
-// Funciones para manejar el formulario de agregar épicas
-function setupAddEpicForm() {
-const form = document.getElementById("add-epic-form");
-form.addEventListener("submit", handleAddEpicSubmit);
-}
-
-function handleAddEpicSubmit(e) {
+  function handleAddEpicSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const epic = {
@@ -167,21 +150,25 @@ function handleAddEpicSubmit(e) {
       dependentSquads: formData.get("dependent-squads"),
       startDate: formData.get("start-date"),
       implementationDate: formData.get("implementation-date"),
+      dependenciesResolved: false
     };
-    epic.dependenciesResolved = false; // Agregar esta línea
-    saveEpic(epic);
+    const epicsRef = firebase.database().ref('epics');
+    epicsRef.push(epic, (error) => {
+      if (error) {
+        console.log("Error adding epic: ", error);
+      } else {
+        console.log("Epic added successfully!");
+      }
+    });
     renderEpicCard(epic);
     e.target.reset();
   }
-  
-  
 
-// Funciones para manejar la tabla de épicas
-function setupEpicTable() {
-// Aquí puedes configurar la tabla de épicas si es necesario
-}
+  function setupEpicTable() {
+    // Configurar la tabla de épicas si es necesario
+  }
 
-function renderEpicTable(epics) {
+  function renderEpicTable(epics) {
     const userData = getUserData();
     const tableContainer = document.querySelector(".epic-table");
     tableContainer.innerHTML = `
@@ -214,21 +201,19 @@ function renderEpicTable(epics) {
             </tbody>
         </table>
     `;
-}
+  }
 
-
-// Funciones para manejar eventos de la página
+  // Funciones para manejar eventos de la página
 function handleViewTableClick() {
-    const epicTable = document.querySelector(".epic-table");
-    if (epicTable.style.display === "block") {
-        epicTable.style.display = "none";
-    } else {
-        const sortedEpics = getEpics().sort((a, b) => new Date(a.implementationDate) - new Date(b.implementationDate));
-        renderEpicTable(sortedEpics);
-        epicTable.style.display = "block";
-    }
+const epicTable = document.querySelector(".epic-table");
+if (epicTable.style.display === "block") {
+epicTable.style.display = "none";
+} else {
+const sortedEpics = getEpics().sort((a, b) => new Date(a.implementationDate) - new Date(b.implementationDate));
+renderEpicTable(sortedEpics);
+epicTable.style.display = "block";
 }
-
+}
 
 // Función principal para configurar la página
 function main() {
@@ -248,3 +233,8 @@ document.getElementById('view-table').addEventListener('click', handleViewTableC
 
 // Ejecutar la función principal
 main();
+
+
+
+
+
